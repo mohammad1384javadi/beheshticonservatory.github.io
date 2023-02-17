4cef2d986a825b62bfb9ba09f959413e67fe874b
mohammad1384javadi <125662447+mohammad1384javadi@users.noreply.github.com>
2023-02-17 18:11:15 +0330

    Add files via upload

---
+++ scripts/gopage.js
@@ -0,0 +1,24 @@
+function cofi(){
+	location.replace("computer.html");
+}
+function bafi(){
+	location.replace("bargh.html");
+}
+function chfi(){
+	location.replace("chob.html");
+}
+function elefi(){
+	location.replace("electronic.html");
+}
+function mafi(){
+	location.replace("mashinab.html");
+}
+function mechfi(){
+	location.replace("mechanic.html");
+}
+function safi(){
+	location.replace("sakhteman.html");
+}
+function tafi(){
+	location.replace("tasisat.html");
+}
