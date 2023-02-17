5fd31d00c4d57d7d07e3015986aacaca4220b170
mohammad1384javadi <125662447+mohammad1384javadi@users.noreply.github.com>
2023-02-17 18:09:27 +0330

    Add files via upload

---
+++ scripts/answer.js
@@ -0,0 +1,37 @@
+var content = document.querySelector(".content");
+var tog = document.querySelector(".toggle");
+	tog.onclick = function(){
+		content.classList.toggle("active");
+	}
+var content1 = document.querySelector(".content1");
+var tog1 = document.querySelector(".toggle1");
+	tog1.onclick = function(){
+		content1.classList.toggle("active");
+	}
+var content2 = document.querySelector(".content2");
+var tog2 = document.querySelector(".toggle2");
+	tog2.onclick = function(){
+		content2.classList.toggle("active");
+	}
+var content3 = document.querySelector(".content3");
+var tog3 = document.querySelector(".toggle3");
+	tog3.onclick = function(){
+		content3.classList.toggle("active");
+	}
+var content4 = document.querySelector(".content4");
+var tog4 = document.querySelector(".toggle4");
+	tog4.onclick = function(){
+		content4.classList.toggle("active");
+	}
+var content5 = document.querySelector(".content5");
+var tog5 = document.querySelector(".toggle5");
+	tog5.onclick = function(){
+		content5.classList.toggle("active");
+	}
+//for(var i = 0, i < 11, i++)
+//	{
+//		var content = document.querySelectorAll(".content")[i];
+//		var tog = document.querySelectorAll(".toggle")[i];
+//		tog[i].onclick = function(){
+//		content[var i].classList.toggle("active");
+//	}
