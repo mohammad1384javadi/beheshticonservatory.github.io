9bb21b427f542b7ab4dcc5aa850f8d34efbe4da5
mohammad1384javadi <125662447+mohammad1384javadi@users.noreply.github.com>
2023-02-17 18:10:03 +0330

    Add files via upload

---
+++ scripts/btnback.js
@@ -0,0 +1,23 @@
+function homepage(){
+	location.replace("../index.html");
+}
+function change(){
+	if (document.body.classList.contains("day")){
+		document.body.classList = "";
+	}
+	else{
+		document.body.className = "day";
+	}
+}
+function comsli(){
+	location.replace("compslider.html");
+}
+function elesli(){
+	location.replace("electroslider.html");
+}
+function chsli(){
+	location.replace("chobslider.html");
+}
+function basli(){
+	location.replace("barghslider.html");
+}
