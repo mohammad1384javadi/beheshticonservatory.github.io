3a171dc2f22e21619ddcf0a1bfcde3c3974e7c39
mohammad1384javadi <125662447+mohammad1384javadi@users.noreply.github.com>
2023-02-17 18:11:39 +0330

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+function gofieldp(){
+	location.replace("pages/fields.html");
+}
+function goofficialp(){
+	location.replace("pages/officials.html");
+}
+function goschooleimg(){
+	location.replace("pages/schoolslider.html");
+}
+function gomenu(){
+	location.replace("index.html");
+}
